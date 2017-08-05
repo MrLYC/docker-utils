ac33137025c72567d5c8461f3d989e776f8bfd77
MrLYC <saber_zodiac@163.com>
2017-08-05 09:39:29 +0000

    update build.sh

--- alpine/build.sh
+++ alpine/build.sh
@@ -3,7 +3,9 @@
 set -x
 
 apk update
-apk add wget ca-certificates unzip python
+apk add wget ca-certificates unzip python py-pip
+
+pip install pytest
 
 wget https://github.com/MrLYC/docker-utils/archive/master.zip
 unzip master.zip
